0926908bab5b9e23894191484e7a1c55a2289dd6
Rakib <hasan.rakib@datasoft-bd.com>
2023-03-13 11:25:42 +0600

    Black Project Push!

---
+++ app/src/main/java/rakib/hasan/whispr/MainActivity.kt
@@ -0,0 +1,11 @@
+package rakib.hasan.whispr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
